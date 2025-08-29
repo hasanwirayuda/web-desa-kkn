91d8f65566a0b5acbb26a35c8d43856509b41dc0
Hasan Wirayuda <hasanwirayuda375@gmail.com>
2025-08-29 23:54:46 +0700

    install nuxtjs/sitemap package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
     server: {
       allowedHosts: [
-        ".ngrok-free.app", // ✅ allow all ngrok tunnels (so it works even if the subdomain changes)
+        '.ngrok-free.app', // ✅ allow all ngrok tunnels (so it works even if the subdomain changes)
       ],
     },
   },
-  modules: ["@nuxt/image", "@nuxt/fonts"],
+  modules: ['@nuxt/image', '@nuxt/fonts', '@nuxtjs/sitemap'],
+  // @ts-expect-error - module typing issue
+  sitemap: {
+    siteUrl: 'https://desapangarengan.vercel.app/',
+  },
   fonts: {
     families: [
       {
-        name: "Poppins",
-        provider: "google", // ambil dari Google Fonts
+        name: 'Poppins',
+        provider: 'google', // ambil dari Google Fonts
         weights: [400, 500, 600, 700], // pilih varian yang kamu perlu
       },
     ],
